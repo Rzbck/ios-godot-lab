7c8f5fbc983522d2114b516014ca528a2e5493e3
Data_C0re_ <10966796+Rzbck@users.noreply.github.com>
2026-09-10 20:07:43 +0200

    feat(tracker): request complete startup permissions

---
+++ apps/watch-sensor-lab/iphone/Sources/StartupPermissionCoordinator.swift
@@ -0,0 +1,105 @@
+import CoreLocation
+import CoreMotion
+import Foundation
+import HealthKit
+
+@MainActor
+final class StartupPermissionCoordinator: NSObject, ObservableObject, CLLocationManagerDelegate {
+    private let healthStore = HKHealthStore()
+    private let locationManager = CLLocationManager()
+    private let pedometer = CMPedometer()
+    private var started = false
+    private var motionProbeStarted = false
+
+    override init() {
+        super.init()
+        locationManager.delegate = self
+    }
+
+    func start() {
+        guard !started else { return }
+        started = true
+        requestHealthThenLocation()
+    }
+
+    private func requestHealthThenLocation() {
+        guard HKHealthStore.isHealthDataAvailable() else {
+            requestLocationThenMotion()
+            return
+        }
+
+        healthStore.requestAuthorization(
+            toShare: [HKObjectType.workoutType()],
+            read: Self.healthReadTypes()
+        ) { [weak self] _, _ in
+            DispatchQueue.main.async {
+                self?.requestLocationThenMotion()
+            }
+        }
+    }
+
+    private func requestLocationThenMotion() {
+        switch locationManager.authorizationStatus {
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        default:
+            requestMotionProbe()
+        }
+    }
+
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        guard started, manager.authorizationStatus != .notDetermined else { return }
+        requestMotionProbe()
+    }
+
+    private func requestMotionProbe() {
+        guard !motionProbeStarted else { return }
+        motionProbeStarted = true
+        guard CMPedometer.isStepCountingAvailable(), CMPedometer.authorizationStatus() == .notDetermined else { return }
+
+        let end = Date()
+        let start = end.addingTimeInterval(-60)
+        pedometer.queryPedometerData(from: start, to: end) { _, _ in }
+    }
+
+    private static func healthReadTypes() -> Set<HKObjectType> {
+        var types: Set<HKObjectType> = [HKObjectType.workoutType()]
+
+        let quantityIdentifiers: [HKQuantityTypeIdentifier] = [
+            .heartRate,
+            .activeEnergyBurned,
+            .distanceWalkingRunning,
+            .distanceCycling,
+            .distanceSwimming,
+            .restingHeartRate,
+            .heartRateVariabilitySDNN,
+            .vo2Max,
+            .stepCount,
+            .appleExerciseTime,
+            .walkingSpeed,
+            .walkingStepLength,
+            .walkingAsymmetryPercentage,
+            .walkingDoubleSupportPercentage,
+            .appleWalkingSteadiness,
+            .runningSpeed,
+            .runningPower,
+            .runningStrideLength,
+            .runningGroundContactTime,
+            .runningVerticalOscillation,
+            .heartRateRecoveryOneMinute,
+            .respiratoryRate,
+        ]
+
+        for identifier in quantityIdentifiers {
+            if let type = HKQuantityType.quantityType(forIdentifier: identifier) {
+                types.insert(type)
+            }
+        }
+
+        if let sleep = HKObjectType.categoryType(forIdentifier: .sleepAnalysis) {
+            types.insert(sleep)
+        }
+
+        return types
+    }
+}
